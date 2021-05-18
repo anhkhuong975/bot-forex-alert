a9a6b730179e15bfba702e6100a4604a601d65d8
khuong <anhkhuong975@gmail.com>
2021-05-18 23:40:54 +0700

    edit const

--- src/constain.ts
+++ src/constain.ts
@@ -1,7 +1,7 @@
-export const TIME_INTERVAL:number = 1200000;
+export const TIME_INTERVAL:number = 2000000;
 export const RETRY_API :number = 3;
-export const RSI_OVER_H = 70;
-export const RSI_OVER_L = 30;
+export const RSI_OVER_H = 80;
+export const RSI_OVER_L = 20;
 export const MAIL_SERVICE_CONFIG = {
     service: 'gmail',
     auth: {
